2f85e21fe87a31f0574b1a6acc737837110b39b4
jbookout98 <jbookout98@gmail.com>
2024-10-23 12:08:00 -0500

    Fixed an issue with the json formatting.

--- src/main/java/org/example/CourseController.java
+++ src/main/java/org/example/CourseController.java
@@ -15,21 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourseController {
 
     @GetMapping("/getCourses")
-    public List<Course> getCourses(@RequestParam(required = false) String crn) {
+    public Course getCourses(@RequestParam(required = false) String crn) {
 
         String scheduleText = PDFReader.extractTextFromPDF("Spring2024.pdf");
 
         List<Course> courses = ScheduleParser.parseCourses(scheduleText);
         System.out.println("Running the Course Controller");
         // Print the full schedule for debugging
-
+        Course toReturnCourse;
         // If a CRN is provided, filter the courses by CRN
         if (crn != null && !crn.isEmpty()) {
             courses = courses.stream()
                     .filter(course -> course.getCrn().equals(crn))
                     .collect(Collectors.toList());
         }
-        System.out.println(courses);
-        return courses;
+        System.out.println(courses.get(0));
+        return courses.get(0);
     }
 }
